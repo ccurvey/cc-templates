6e3f9bcb9274c6ae8959334a21f3286676e42a78
Chris Curvey <chris@chriscurvey.com>
2023-05-29 05:37:02 -0400

    added create view

--- django-pytest/{{cookiecutter.project_name}}/website/{{cookiecutter.project_name}}/views.py
+++ django-pytest/{{cookiecutter.project_name}}/website/{{cookiecutter.project_name}}/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .models import Person
+from django.urls import reverse_lazy
+from django.contrib import messages
 from vanilla import ListView, DetailView, CreateView
 
+from .models import Person
 
-# Create your views here.
+
 def index(request):
     return render(request, "fakeout/index.html")
 
@@ -27,9 +29,37 @@ class PersonProxy(Person):
             for field in self.__class__._meta.fields
         ]
 
+    def __str__(self):
+        return self.first_name
 
+
+# doing as much as I can through configuration
 class PersonDetailView(DetailView):
     model = PersonProxy
     template_name = 'fakeout/person_detail.html'
     lookup_field = "uuid"
     lookup_url_kwarg = 'person_uuid'
+
+
+class PersonCreateView(CreateView):
+    model = PersonProxy
+    fields = [
+        'first_name',
+        'last_name',
+        'email',
+        'company',
+        'address',
+        'city',
+        'state',
+        'postal_code',
+    ]
+
+    template_name = 'fakeout/person_create.html'
+    success_url = reverse_lazy("nineteen:person-list")
+
+    def form_valid(self, *args, **kwargs):
+        ret = super().form_valid(*args, **kwargs)
+
+        messages.success(self.request, f"{self.object} has been created")
+
+        return ret
